1d916ef4f1a87f5fb52ded5d51bea582ccd63899
Genaro Bedenko <genaro.bedenko@gmail.com>
2019-12-07 11:01:07 +0000

    Implemented easy and hard difficulties

--- src/game.py
+++ src/game.py
@@ -4,6 +4,7 @@
 from winning_states import WinningStates
 from move import Move
 from player import HumanPlayer, ComputerPlayer
+from random import choice
 
 class Game():
 
@@ -16,6 +17,9 @@ class Game():
 					 0, 0, 0,
 					 0, 0, 0,
 					 0, 0, 0]
+
+		self.CROSSES_SYMBOL = "X"
+		self.NOUGHTS_SYMBOL = "O"
 							 
 
 	def clear_board(self):
@@ -125,28 +129,125 @@ class Game():
 
 		if isinstance(player, HumanPlayer):
 
-			player_move = input(player.name + ", enter grid position: ")
-			self.update_grid(Move(int(player_move), player.symbol))
+			chosen_move = int(input(player.name + ", enter grid position: "))
 		
 		elif isinstance(player, ComputerPlayer):
 			
+			# List for possible moves/ empty squares
+			possible_moves = []
+
+			# If it doesn't contain a 1 or 2 then it is empty and can be considered a possible move
+			for i in range(1,10):
+				if (self.grid[i] != "X") and (self.grid[i] != "O"):
+					possible_moves.append(i)
+
 			if player.difficulty == 1:
-				# TODO Random moves
-				computer_move = 8
-				self.update_grid(Move(int(computer_move), player.symbol))
+				# Random move
+				chosen_move = choice(possible_moves)
 
 			elif player.difficulty == 2:
-				# TODO Hard implemented moves, except possible lucky ways
-				computer_move = 7
-				self.update_grid(Move(int(computer_move), player.symbol))
+				# Hard implemented moves, can win with luck
+				
+				# For each possible turn the human made, decide the best possible moves to go for the first turn
+				# that will give the computer the most possible winning scenarios at this stage of the game
+				if len(possible_moves)==8: 
+					if self.grid[1] != 0:
+						possible_moves = [5]
+					elif self.grid[2] != 0:
+						possible_moves = [5,7,9]
+					elif self.grid[3] != 0:
+						possible_moves = [5]
+					elif self.grid[4] != 0:
+						possible_moves = [3,5,9]
+					elif self.grid[5] != 0:
+						possible_moves = [1,3,7,9]
+					elif self.grid[6] != 0:
+						possible_moves = [1,5,7]
+					elif self.grid[7] != 0:
+						possible_moves = [5]
+					elif self.grid[8] != 0:
+						possible_moves = [1,3,5]
+					elif self.grid[9] != 0:
+						possible_moves = [5]
+
+				elif len(possible_moves)==6:
+
+					# If the human has gone in two opposite corners, then if the computer's next turn is in a corner then it is going to lose
+					# So this check means that it will not go in a corner in this situation, and mean it has the best chance of winning
+					if (self.grid[1]=="X") and (self.grid[9]=="X") or (self.grid[3]=="X") and (self.grid[7]=="X"):
+						possible_moves = [2,4,6,8]
+
+					# If the human has had two moves and all the symbols are in a diagonal line, then the computer should go in one of the 
+					# remaining corners, otherwise it is making a move that will mean it loses, and mean it has the best chance of winning
+					if (self.grid[1]=="X") and (self.grid[5]=="X") and (self.grid[9]=="O"):
+						possible_moves = [3,7]
+					if (self.grid[3]=="X") and (self.grid[5]=="X") and (self.grid[7]=="O"):
+						possible_moves = [1,9]
+					if (self.grid[7]=="X") and (self.grid[5]=="X") and (self.grid[3]=="O"):
+						possible_moves = [1,9]
+					if (self.grid[9]=="X") and (self.grid[5]=="X") and (self.grid[1]=="O"):
+						possible_moves = [3,7]
+
+				chosen_move = choice(possible_moves)
+
+				# For each of the 9 grid squares, check to see if it has to be played for the computer to win the game
+				# But also make sure that this grid square hasn't already been played first as well
+				if((self.grid[2]=="O" and self.grid[3]=="O") or (self.grid[4]=="O" and self.grid[7]=="O") or (self.grid[5]=="O" and self.grid[9]=="O")) and (self.grid[1] != 1) and (self.grid[1] != 2):
+						chosen_move = 1
+				elif((self.grid[1]=="O" and self.grid[3]=="O") or (self.grid[5]=="O" and self.grid[8]=="O")) and (self.grid[2] != 1) and (self.grid[2] != 2):
+						chosen_move = 2
+				elif((self.grid[1]=="O" and self.grid[2]=="O") or (self.grid[6]=="O" and self.grid[9]=="O") or (self.grid[5]=="O" and self.grid[7]=="O")) and (self.grid[3] != 1) and (self.grid[3] != 2):
+						chosen_move = 3
+				elif((self.grid[1]=="O" and self.grid[7]=="O") or (self.grid[5]=="O" and self.grid[6]=="O")) and (self.grid[4] != 1) and (self.grid[4] != 2):
+						chosen_move = 4
+				elif((self.grid[1]=="O" and self.grid[9]=="O") or (self.grid[2]=="O" and self.grid[8]=="O") or (self.grid[3]=="O" and self.grid[7]=="O") or (self.grid[4]=="O" and self.grid[6]=="O")) and (self.grid[5] != 1) and (self.grid[5] != 2):
+						chosen_move = 5
+				elif((self.grid[3]=="O" and self.grid[9]=="O") or (self.grid[4]=="O" and self.grid[5]=="O")) and (self.grid[6] != 1) and (self.grid[6] != 2):
+						chosen_move = 6
+				elif((self.grid[1]=="O" and self.grid[4]=="O") or (self.grid[8]=="O" and self.grid[9]=="O") or (self.grid[3]=="O" and self.grid[5]=="O")) and (self.grid[7] != 1) and (self.grid[7] != 2):
+						chosen_move = 7
+				elif((self.grid[2]=="O" and self.grid[5]=="O") or (self.grid[7]=="O" and self.grid[9]=="O")) and (self.grid[8] != 1) and (self.grid[8] != 2):
+						chosen_move = 8
+				elif((self.grid[3]=="O" and self.grid[6]=="O") or (self.grid[1]=="O" and self.grid[5]=="O") or (self.grid[7]=="O" and self.grid[8]=="O")) and (self.grid[9] != 1) and (self.grid[9] != 2):
+						chosen_move = 9
+
+				# For each of the 9 grid squares, check to see if it has to be played to block the player from winning
+				# But also make sure that this grid square hasn't already been played first as well
+				elif((self.grid[2]=="X" and self.grid[3]=="X") or (self.grid[4]=="X" and self.grid[7]=="X") or (self.grid[5]=="X" and self.grid[9]=="X")) and (self.grid[1] != 1) and (self.grid[1] != 2):
+						chosen_move = 1
+				elif((self.grid[1]=="X" and self.grid[3]=="X") or (self.grid[5]=="X" and self.grid[8]=="X")) and (self.grid[2] != 1) and (self.grid[2] != 2):
+						chosen_move = 2
+				elif((self.grid[1]=="X" and self.grid[2]=="X") or (self.grid[6]=="X" and self.grid[9]=="X") or (self.grid[5]=="X" and self.grid[7]=="X")) and (self.grid[3] != 1) and (self.grid[3] != 2):
+						chosen_move = 3
+				elif((self.grid[1]=="X" and self.grid[7]=="X") or (self.grid[5]=="X" and self.grid[6]=="X")) and (self.grid[4] != 1) and (self.grid[4] != 2):
+						chosen_move = 4
+				elif((self.grid[1]=="X" and self.grid[9]=="X") or (self.grid[2]=="X" and self.grid[8]=="X") or (self.grid[3]=="X" and self.grid[7]=="X") or (self.grid[4]=="X" and self.grid[6]=="X")) and (self.grid[5] != 1) and (self.grid[5] != 2):
+						chosen_move = 5
+				elif((self.grid[3]=="X" and self.grid[9]=="X") or (self.grid[4]=="X" and self.grid[5]=="X")) and (self.grid[6] != 1) and (self.grid[6] != 2):
+						chosen_move = 6
+				elif((self.grid[1]=="X" and self.grid[4]=="X") or (self.grid[8]=="X" and self.grid[9]=="X") or (self.grid[3]=="X" and self.grid[5]=="X")) and (self.grid[7] != 1) and (self.grid[7] != 2):
+						chosen_move = 7
+				elif((self.grid[2]=="X" and self.grid[5]=="X") or (self.grid[7]=="X" and self.grid[9]=="X")) and (self.grid[8] != 1) and (self.grid[8] != 2):
+						chosen_move = 8
+				elif((self.grid[3]=="X" and self.grid[6]=="X") or (self.grid[1]=="X" and self.grid[5]=="X") or (self.grid[7]=="X" and self.grid[8]=="X")) and (self.grid[9] != 1) and (self.grid[9] != 2):
+						chosen_move = 9
 
 			elif player.difficulty == 3:
 				# TODO Hard implemented moves for all cases, player can draw at best
-				computer_move = 9
-				self.update_grid(Move(int(computer_move), player.symbol))
+				chosen_move = choice(possible_moves)
 			
 		else:
 			raise TypeError("Incorrect player type created")
+
+		# Check that this move is valid, and hasn't already been made
+		if (self.grid[chosen_move] == "X") or (self.grid[chosen_move] == "O"):
+			
+			print("Invalid Move. This grid square has already been used. Please select another")
+			self.player_turn(player)
+
+		else:
+			self.update_grid(Move(int(chosen_move), player.symbol))
+
 
 
 class SinglePlayerGame(Game):
