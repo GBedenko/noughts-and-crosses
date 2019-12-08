f6ec4a79a28dbe5bc4b9d2131ea4f7910677cecd
Genaro Bedenko <genaro.bedenko@gmail.com>
2019-12-08 22:10:42 +0000

    Removed option to save/load game, not necessary

--- play_game.py
+++ play_game.py
@@ -10,17 +10,3 @@ if __name__ == "__main__":
 	setup_ui()
 	new_game = Game().select_game_mode()
 	new_game.start()
-		
-	# playSavedGame = messagebox.askquestion(title="Play Previous Game?", message="Do you want to play a previously saved game?")
-
-	# if(playSavedGame=="yes"):
-	# 	try:
-	# 		loadGame(gridSquares)
-
-	# 	# If the user clicks yes to play a saved game but their isn't one saved in the directory. Display a message to tell them
-	# 	# this and move on to starting a new game
-	# 	except FileNotFoundError:
-	# 		messagebox.showinfo(title="No Saved Game Available", message="There isn't a currently saved game available to play")
-	# 		gameModeSelection()
-	# else:
-	# 	gameModeSelection()
